c52a63cc8d9a3b6d553e13fee246113c9598cf8e
Lukas Martinelli <me@lukasmartinelli.ch>
2014-07-29 07:40:51 +0200

    Removed PHP install script and added more collision keys

--- install_php.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget http://museum.php.net/php5/php-5.3.8.tar.gz
-tar zxf php-5.3.8.tar.gz
-cd php-5.3.8
-./configure --disable-libxml --disable-dom --disable-simplexml --disable-xml --disable-xmlreader --disable-xmlwriter --without-pear
-make
-sudo make install
